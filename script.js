d4bf65b1356e1b7d8277c9d31bbbf3f6f2a8bc15
gbalciunas <gintaras.balciunas71@gmail.com>
2022-05-30 03:09:18 +0300

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
+-------------------------------------------------------------------------------------------- */
+import { one, two, three, four, five } from "./modules/numbers/numbers.js"
+
+ let a = composition(one, four);
+import { composition } from "../modules/math/composition.js"
+
+ let b = division(four, two);
+import { division } from "../modules/math/division.js"
+
+ let c = substraction(three, two);
+import { substraction } from "../modules/math/substraction.js"
+
+ let d = multiplication(five, two);
+import { multiplication } from "../modules/math/multiplication.js"
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
